71d5659ab4c6fff75937d8c73d52bdeb5ed40e7e
Jack Foley <jackfdevelopment@gmail.com>
2024-10-07 12:08:22 +0100

    Update comments.

--- lab-four/main.go
+++ lab-four/main.go
@@ -1,27 +1,3 @@
-//Barrier.go Template Code
-//Copyright (C) 2024 Mr. Jack Foley
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-//--------------------------------------------
-// Author: Jack Foley (C00274246@setu.ie)
-// Created on 30/9/2024
-// Modified by: Jack Foley
-// Issues: N/A
-// The barrier is not implemented!
-//--------------------------------------------
-
 package main
 
 import (
